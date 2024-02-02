c90385317aceab3347c228dcd787285924b0dd62
anesta95 <adriannesta@gmail.com>
2024-02-02 16:57:16 -0500

    adding to annualize custom functions

--- custom_functions/annualize_vec.R
+++ custom_functions/annualize_vec.R
@@ -33,3 +33,37 @@ dplyr::mutate(df, dplyr::across(dplyr::where(is.numeric), ~annualize_vec(.x)))
 
 # 3. Create two new index columns
 dplyr::mutate(df, dplyr::across(dplyr::where(is.numeric), ~annualize_vec(.x), .names = "ann_{.col}"))
+
+annualize_vals <- function(dec_val, 
+                           later_val, 
+                           time_period = "monthly",
+                           period_number) {
+  # Things this function needs to do:
+  # 1. Check if both `dec_val` and `later_val` are numeric
+  # 2. Calculate annualized change in the numeric values based on time period. 
+  # 3. Return new annualized value.
+  
+  if(!is.numeric(dec_val) | !is.numeric(later_val)) {
+    stop("The supplied object to the `dec_val` and `later_val` arguments are not numeric",
+         call. = T)
+  }
+  
+  if(!(period_number %in% 1:12)) {
+    stop("The supplied object to the `period_number` argument is not a integer from 1 to 12",
+         call. = T)
+  }
+  
+  if(!time_period %in% c("monthly", "quarterly")) {
+    stop("The supplied object to the `time_period` argument is not either \"monthly\", or \"quarterly\"",
+         call. = T)
+  }
+  
+  if(time_period == "quarterly") {
+    ann_val <- (((later_val / dec_val) ^ (4 / period_number)) - 1) * 100  
+  } else {
+    ann_val <- (((later_val / dec_val) ^ (12 / period_number)) - 1) * 100
+  }
+
+  return(ann_val)  
+  
+}
