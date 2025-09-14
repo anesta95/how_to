1d32a149b463d67bd1996168193c24793929f91a
anesta95 <adriannesta@gmail.com>
2025-09-14 12:39:22 -0400

    finished productive R workflow lesson 2

--- R/productive_R_workflow/1_clean_your_code/analysis.R
+++ R/productive_R_workflow/1_clean_your_code/analysis.R
@@ -1,37 +1,56 @@
 library(readr)
 library(dplyr)
 library(ggplot2)
+library(tidyr)
 
 # Old: Pre Module 1
 data <- read.csv("https://raw.githubusercontent.com/holtzy/R-graph-gallery/master/DATA/data_2.csv")
 
 summary(data)
 
-print(round(mean(subset(na.omit(data), species == "Adelie" & island == "Torgersen")$bill_length_mm),2))
-print(round(mean(subset(na.omit(data), species == "Adelie" & island == "Biscoe")$bill_length_mm),2))
-print(round(mean(subset(na.omit(data), species == "Adelie" & island == "Dream")$bill_length_mm),2))
+print(round(mean(subset(na.omit(data), species == "Adelie" & island == "Torgersen")$bill_length_mm), 2))
+print(round(mean(subset(na.omit(data), species == "Adelie" & island == "Biscoe")$bill_length_mm), 2))
+print(round(mean(subset(na.omit(data), species == "Adelie" & island == "Dream")$bill_length_mm), 2))
 
 
 # Plot
-penguins_clean <- na.omit(   data  )
-plot(penguins_clean$bill_length_mm, penguins_clean$bill_depth_mm, type='n', xlab='Bill Length (mm)', ylab='Bill Depth (mm)', main='Penguin Bill Dimensions')
+penguins_clean <- na.omit(data)
+plot(penguins_clean$bill_length_mm, penguins_clean$bill_depth_mm, type = "n", xlab = "Bill Length (mm)", ylab = "Bill Depth (mm)", main = "Penguin Bill Dimensions")
 points(
-  penguins_clean$bill_length_mm[penguins_clean$species  ==  "Adelie"], penguins_clean$bill_depth_mm[penguins_clean$species == "Adelie"], col='red', pch=16)
-points(penguins_clean$bill_length_mm[penguins_clean$species == "Chinstrap"], penguins_clean$bill_depth_mm[penguins_clean$species == "Chinstrap"], col='green', pch=17)
+  penguins_clean$bill_length_mm[penguins_clean$species == "Adelie"], penguins_clean$bill_depth_mm[penguins_clean$species == "Adelie"],
+  col = "red", pch = 16
+)
+points(penguins_clean$bill_length_mm[penguins_clean$species == "Chinstrap"], penguins_clean$bill_depth_mm[penguins_clean$species == "Chinstrap"], col = "green", pch = 17)
 points(penguins_clean$bill_length_mm[penguins_clean$species == "Gentoo"],
-       penguins_clean$bill_depth_mm[penguins_clean$species == "Gentoo"], col='blue', pch=18)
-legend("topright", legend=unique(penguins_clean$species),
-       col=c('red'
-             , 'green',
-             'blue'), pch=c(16, 17, 18))
+  penguins_clean$bill_depth_mm[penguins_clean$species == "Gentoo"],
+  col = "blue", pch = 18
+)
+legend("topright",
+  legend = unique(penguins_clean$species),
+  col = c(
+    "red",
+    "green",
+    "blue"
+  ), pch = c(16, 17, 18)
+)
 
 
 # New: Homework after Module 1
 # All one pipe!
-readr::read_csv(
+read_csv(
   file = "https://raw.githubusercontent.com/holtzy/R-graph-gallery/master/DATA/data_2.csv"
-) |> 
-  filter(across(everything(), ))
-
-
-
+) |>
+  drop_na() |>
+  ggplot(aes(x = bill_length_mm, y = bill_depth_mm)) +
+  geom_point(aes(color = species, shape = species)) +
+  labs(
+    title = "Penguin Bill Dimensions",
+    x = "Bill Depth (mm)",
+    y = "Bill Length (mm)",
+  ) +
+  theme_classic() +
+  theme(
+    legend.title = element_blank(),
+    legend.position = "top",
+    legend.justification = "right"
+  )
