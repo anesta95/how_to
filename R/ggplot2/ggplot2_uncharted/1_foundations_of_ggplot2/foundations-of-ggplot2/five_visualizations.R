3a0726685cdd84f0198f388661a4922b0a314c3f
anesta95 <adriannesta@gmail.com>
2025-09-25 18:19:37 -0400

    adding another viz

--- R/ggplot2/ggplot2_uncharted/1_foundations_of_ggplot2/foundations-of-ggplot2/five_visualizations.R
+++ R/ggplot2/ggplot2_uncharted/1_foundations_of_ggplot2/foundations-of-ggplot2/five_visualizations.R
@@ -27,26 +27,31 @@ penguins_ggplot2_no_sex_na <- penguins_ggplot2 |>
 penguins_ggplot2_plt <- penguins_ggplot2_no_sex_na |> 
   ggplot(aes(x = body_mass_g, y = bill_depth_mm)) +
   geom_point(aes(color = species)) +
-  geom_smooth(aes(color = species)) +
+  geom_smooth(aes(color = species, fill = species)) +
   facet_wrap(~sex) +
   theme_bw() +
   labs(
     title = "Penguin Body Mass and Bill Depth by Sex",
     subtitle = "Body mass correlates most with bill depth for female adelie and chinstrap penguins",
     caption = "Adult pengins on islands in the Palmer Archipelago, Antarctica from ggplot2 penguins dataset.",
+    color = "Species",
     x = "Body Mass (grams)",
     y = "Bill Depth (millimeters)"
-    
   ) +
+  guides(color = guide_legend(override.aes=list(fill=NA)),
+         fill = "none") +
   theme(
     plot.title = element_text(face = "bold", size = 32),
     plot.subtitle = element_text(face = "bold", size = 20),
-    plot.caption = element_text(size = 10),
+    plot.caption = element_text(size = 10, margin = margin(10, 3, 3, 3, "pt")),
     plot.margin = margin(20, 100, 20, 20, "pt"),
-    axis.title = element_text(size = 12, margin = margin(10, 10, 25, 10, "pt")),
+    axis.title = element_text(size = 12, margin = margin(10, 10, 10, 10, "pt")),
     axis.text = element_text(face = "bold", size = 16),
     strip.text = element_text(face = "bold", size = 16),
-    legend.position = "none"
+    legend.title = element_text(face = "bold", size = 18),
+    legend.text = element_text(size = 16),
+    legend.key = element_rect(fill = "white"),
+    legend.position = "top"
   )
 
 # Saving plot
@@ -59,7 +64,43 @@ ggsave(
     dpi = 72
 )
 
+# Plot 2: Grouped bar flipper_length_mm for each species faceted by year
+penguins_ggplot2_plt_2 <- penguins_ggplot2_no_sex_na |> 
+  ggplot(aes(x = species, y = flipper_length_mm)) +
+  coord_cartesian(ylim = c(150, NA)) +
+  geom_bar(aes(fill = sex),
+           stat = "summary",
+           fun = "mean",
+           position = position_dodge(width = .8)) +
+  scale_y_continuous(expand = expansion(mult = c(0, .15))) +
+  facet_wrap(~year) +
+  theme_bw() +
+  labs(
+    title = "Flipper Length by Penguin Species",
+    subtitle = "From '07 - '09. Gentoo penguins consistently larger than Adelie and Chinstrap penguins.",
+    caption = "Adult pengins on islands in the Palmer Archipelago, Antarctica from ggplot2 penguins dataset.",
+    y = "Length (millimeters)"
+  ) +
+  theme(
+    plot.title = element_text(face = "bold", size = 32),
+    plot.subtitle = element_text(face = "bold", size = 20),
+    plot.caption = element_text(size = 10, margin = margin(10, 3, 3, 3, "pt")),
+    plot.margin = margin(20, 100, 20, 20, "pt"),
+    axis.title.y = element_text(size = 12, margin = margin(10, 10, 10, 10, "pt")),
+    axis.title.x = element_blank(),
+    axis.text = element_text(face = "bold", size = 16),
+    strip.text = element_text(face = "bold", size = 16),
+    legend.title = element_blank(),
+    legend.text = element_text(face = "bold", size = 16),
+    legend.position = "top"
+  )
 
-
-
-
+# Saving plot
+ggsave(
+  filename = "./visualizations/penguins_2_ggplot2_uncharted_module_1_project.png",
+  plot = penguins_ggplot2_plt_2,
+  width = 1200,
+  height = 675,
+  units = "px",
+  dpi = 72
+)
